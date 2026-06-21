68b386fc18ed3f3a99a18a9b228d6eb313b22161
mubarak salisu <marksmanl619@gmail.com>
2026-06-21 23:20:52 +0100

    Feature(app module): add chat endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  chat() {
+    return {
+      message: 'This is a chat response from the server.',
+    };
+  }
 }
